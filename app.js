b5112ec0a1d7a58f471bc0551653288511b13e5d
vgmda <vasilegancin@gmail.com>
2023-01-14 10:04:58 +0000

    Implement db.collection()

--- app.js
+++ app.js
@@ -18,8 +18,29 @@ connectToDb((err) => {
 })
 
 
-
 // Routes ( GET )
 app.get('/books', (req, res) => {
+
+    // Empty array to store all the returned books
+    let book = []
+
+    // find() returns a cursor 
+    // Next step is to fetch the cursor (data) using toArray or forEach
+    // forEach is used to push the books to an array
+    // forEach is async, needs time to finish, when finished then() is executed next
+    // .json will display the books array in a json format
+
+    db.collection('books')
+        .find()
+        .sort({ title: 1 })
+        .forEach(book => books.push(book))
+        .then(() => {
+            res.status(200).json(books)
+        })
+        .catch(() => {
+            res.status(500).json({ error: "Could not fetch the documents" })
+        })
+
     res.json({ mssg: "*** API is now working ***" })
 })
+
