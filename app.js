6aa76fc39795d3462ccbeba7cb08346863408517
vgmda <vasilegancin@gmail.com>
2023-01-14 09:51:50 +0000

    Import functions within app.js

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const { connectToDb, getDb } = require('./db')
 
 // Init & middleware
 const app = express()
 
-// Use local port 3000
-app.listen(3000, () => {
-    console.log('*** Listening to Port: 3000 ***')
+// Connect to Db
+let db
+
+connectToDb((err) => {
+    if (!err) {
+        // Use local port 3000
+        app.listen(3000, () => {
+            console.log('*** Listening to Port: 3000 ***')
+        })
+        db = getDb()
+    }
 })
+
+
 
 // Routes ( GET )
 app.get('/books', (req, res) => {
--- db.js
+++ db.js
@@ -3,14 +3,16 @@ const { MongoClient } = require('mongodb')
 let dbConnection
 
 module.exports = {
-    connectToDb: () => {
+    connectToDb: (cb) => {
         MongoClient.connect('mongodb://localhost:27017/bookstore')
             .then((client) => {
                 dbConnection = client.db()
+                return cb()
             })
             .catch(err => {
                 console.log(err)
+                return cb(err)
             })
     },
-    getDb: () => { }
+    getDb: () => dbConnection
 }
